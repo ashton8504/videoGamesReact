cba52105eda3d0c41deb1bb2b93394652e7b8f7d
ashton8504 <ashtonlittle8504@gmail.com>
2023-12-29 08:38:54 -0500

    saving progress, updated navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,53 +1,55 @@
 import React from "react";
 import styled from "styled-components";
-import { Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-
-// Styled Components
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const StyledNavbarBrand = styled(Navbar.Brand)`
   font-size: 1.5rem;
   margin-top: 0.5rem;
   font-family: "RetroFont", sans-serif;
-  margin-left: 3rem;
-
-  /* Mobile styles */
-  @media (max-width: 768px) {
-    font-size: 1rem;
-    width: 100%;
-    text-align: center;
-  }
+  padding-left: 15px;
 `;
 
 const StyledNavLink = styled(NavLink)`
   color: white;
   margin-right: 1rem;
   text-decoration: none;
-
-  &:hover {
-    color: lightgray;
-  }
-
-  &.active {
-    font-weight: bold;
-  }
+  margin-top: 3px;
+  font-family: "RetroFont", sans-serif;
+  font-size: 1.2rem;
+  ${"" /* border: 2px solid pink; */}
 `;
 
 export default function CustomNavbar() {
   return (
     <Navbar bg="dark" variant="dark">
-      <StyledNavbarBrand>
-        Memorable Characters, Timeless Games
-      </StyledNavbarBrand>
-      <div>
-        <StyledNavLink exact to="/" activeClassName="active">
-          Home
-        </StyledNavLink>
-        <StyledNavLink to="/secondpage" activeClassName="active">
-          Second Page
-        </StyledNavLink>
-        {/* Add more navigation links as needed */}
-      </div>
+      <Container>
+        <StyledNavbarBrand>
+          Memorable Characters, Timeless Games
+        </StyledNavbarBrand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <StyledNavLink exact to="/" activeClassName="active">
+              Home
+            </StyledNavLink>
+            <NavDropdown title="Menu" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
