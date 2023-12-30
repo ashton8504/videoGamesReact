79583f0057a9bf21ea718982251e97bf4fe505a2
ashton8504 <ashtonlittle8504@gmail.com>
2023-12-30 08:31:27 -0500

    saving progress

--- src/Components/Origins/GamingOrigins.js
+++ src/Components/Origins/GamingOrigins.js
@@ -1,9 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+//Styled Components
+
+const StyledHeader = styled.h1`
+  font-size: 3rem;
+  font-family: "RetroFont", sans-serif;
+  text-align: center;
+  margin: 2rem auto;
+`;
 
 export default function GamingOrigins() {
   return (
-    <div>
+    <StyledHeader>
       <h1>How it all began...</h1>
-    </div>
+    </StyledHeader>
   );
 }
