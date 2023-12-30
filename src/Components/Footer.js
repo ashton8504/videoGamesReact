9db3d78c58e6355a77571a08a0fb70f9338a9409
ashton8504 <ashtonlittle8504@gmail.com>
2023-12-30 10:03:54 -0500

    saving progress updated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,16 +6,55 @@ const StyledFooter = styled.footer`
   bottom: 0;
   width: 100%;
   background-color: #060f0a;
-  color: white;
+  color: hsl(154, 84%, 70%);
+  text-shadow: 0 0 4px hsl(154, 84%, 70%);
   text-align: center;
   padding: 10px 0;
   font-family: "RetroFont", sans-serif;
+  position: relative;
+
+  &:hover {
+    .social-links {
+      opacity: 1;
+      visibility: visible;
+    }
+
+    .controller-img {
+      opacity: 0;
+      visibility: hidden;
+    }
+  }
+`;
+
+const StyledSocial = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  opacity: 0;
+  visibility: hidden;
+  transition: opacity 0.3s ease, visibility 0.3s ease;
 `;
 
 const StyledFooterImage = styled.img`
   height: 150px;
   width: 150px;
   filter: invert(80%) hue-rotate(180deg);
+  transition: filter 0.3s ease;
+`;
+
+const StyledSocialLink = styled.a`
+  margin: 0 10px;
+  filter: invert(80%) hue-rotate(180deg);
+
+  transition: filter 0.3s ease;
+
+  &:hover {
+    filter: none;
+  }
 `;
 
 const StyledFooterHeader = styled.h4`
@@ -24,16 +63,24 @@ const StyledFooterHeader = styled.h4`
 
 export default function Footer() {
   return (
-    <div>
-      <div>
-        <StyledFooter>
-          <StyledFooterHeader>Know Your Roots Inc.</StyledFooterHeader>
-          <StyledFooterImage
-            src="imgs/nintendo-controller.svg"
-            alt="Nintendo Controller"
-          />
-        </StyledFooter>
-      </div>
-    </div>
+    <StyledFooter>
+      <StyledFooterImage
+        src="imgs/nintendo-controller.svg"
+        alt="Nintendo Controller"
+        className="controller-img"
+      />
+      <StyledSocial className="social-links">
+        <StyledSocialLink href="https://github.com/ashton8504" target="_blank">
+          <img src="imgs/github.svg" alt="Github" width="50" height="50" />
+        </StyledSocialLink>
+        <StyledSocialLink
+          href="https://www.linkedin.com/in/ashton-codes/"
+          target="_blank"
+        >
+          <img src="imgs/linkedin.svg" alt="LinkedIn" width="55" height="55" />
+        </StyledSocialLink>
+      </StyledSocial>
+      <StyledFooterHeader>Know Your Roots Inc.</StyledFooterHeader>
+    </StyledFooter>
   );
 }
